8896b23cb9652e90caed337012d6d1a0386b2793
bela <nagy.bela.budapest@gmail.com>
2023-09-08 16:07:29 +0200

    flask server

---
+++ flasktest.py
@@ -0,0 +1,12 @@
+''' Flask server inicializálása'''
+
+from flask import Flask
+
+app= Flask(__name__)
+@app.route('/')
+
+def index():
+   return " igyál kávát"
+
+app.run(host="0.0.0.0", port=8080)
+ 
